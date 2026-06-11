0f4a6c87d83f60aad921ba3bb266ded3bddf18cb
Flavio Mammoliti <flavio.mammoliti@gmail.com>
2026-06-11 11:04:01 +0200

    3 - Added Weapon SkeletalMeshComponent to AuraCharacterBase and created BP_AuraCHaracter & BP_Goblin_Spear

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -3,10 +3,14 @@
 
 #include "Character/AuraCharacterBase.h"
 
-
+// constructor
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
+	
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
