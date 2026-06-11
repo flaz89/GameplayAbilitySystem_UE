6f2d692de9a1abbcfb469ac0e6e1c7f5c3b0edeb
Flavio Mammoliti <flavio.mammoliti@gmail.com>
2026-06-11 12:19:48 +0200

    5 - Added module "Enhanched Input", created AurPlayerController class where i implemented MappingContext and some Input Mode on BeginPlay()

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,31 @@
+// Copyright Flavio
+
+
+#include "Player/AuraPlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	check(AuraContext); // if no AuraContext set, it's gonna crash
+	
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+	
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+	
+}
