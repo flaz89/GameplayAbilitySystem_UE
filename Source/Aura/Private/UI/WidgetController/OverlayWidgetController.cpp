df34b6118a7a84c6e2d4a81eb454b19a64b7fb5b
Flavio Mammoliti <flavio.mammoliti@gmail.com>
2026-07-21 12:45:17 +0200

    added BroadcastInitialValues to WidgetController, implemented initial attribute broadcasting in OverlayWidgetController, and updated related Blueprints

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -2,3 +2,27 @@
 
 
 #include "UI/WidgetController/OverlayWidgetController.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
+/**
+ * Broadcasts the initial values of health and max health to their respective listeners.
+ *
+ * This method gets the attribute set from the associated AttributeSet property, extracts
+ * the current health and max health values, and broadcasts these values via the OnHealthChanged
+ * and OnMaxHealthChanged delegates.
+ *
+ * It is primarily used to initialize the UI or other systems that rely on receiving initial
+ * attribute states.
+ *
+ * Note:
+ * - The method expects the AttributeSet property to be properly set and castable to UAuraAttributeSet.
+ * - The OnHealthChanged and OnMaxHealthChanged delegates must be bound appropriately to handle the broadcasts.
+ */
+void UOverlayWidgetController::BroadcastInitialValues()
+{
+	const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(AttributeSet);
+	
+	OnHealthChanged.Broadcast(AuraAttributeSet->GetHealth());
+	OnMaxHealthChanged.Broadcast(AuraAttributeSet->GetMaxHealth());
+}
